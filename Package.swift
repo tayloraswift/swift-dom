087d31468f06691f54e29eba043627c51de57b5b
kelvin13 <kelvin13ma@gmail.com>
2022-07-12 18:19:04 -0500

    remove swift-resource dependency, simplify file naming convention

--- Package.swift
+++ Package.swift
@@ -2,54 +2,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-structured-document",
+    name: "swift-dom",
     products: 
     [
         .library(name: "DOM", targets: ["HTML", "SVG", "RSS"]),
     ],
     dependencies: 
     [
-        .package(url: "https://github.com/kelvin13/swift-resource.git", from: "0.1.6"),
     ],
     targets: 
     [
-        .target(name: "DOM", 
-            dependencies: 
-            [
-                .product(name: "Resource", package: "swift-resource"),
-            ], 
-            path: "sources/dom", 
-            exclude: 
-            [
-            ]),
+        .target(name: "DOM"),
         .target(name: "HTML",
             dependencies: 
             [
-                .product(name: "Resource", package: "swift-resource"),
                 .target(name: "DOM")
-            ],
-            path: "sources/html",
-            exclude: 
-            [
             ]),
         .target(name: "RSS",
             dependencies: 
             [
                 .target(name: "DOM"),
-            ],
-            path: "sources/rss",
-            exclude: 
-            [
             ]),
         .target(name: "SVG",
             dependencies: 
             [
                 .target(name: "DOM"),
                 .target(name: "HTML")
-            ],
-            path: "sources/svg",
-            exclude: 
-            [
             ]),
     ]
 )
