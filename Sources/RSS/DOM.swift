cfd83f7d75008f5ba372e6baa457825ab4478be4
kelvin13 <kelvin13ma@gmail.com>
2022-07-16 15:57:31 -0500

    add missing HTML element cases, and enable rendering RSS XML roots

--- Sources/RSS/DOM.swift
+++ Sources/RSS/DOM.swift
@@ -1,16 +1,16 @@
-@_exported import DOM
+@_exported import DOM 
 
-extension DOM.Root where Domain == HTML
+extension DOM.Root where Domain == RSS
 {
     @inlinable public 
     func rendered<UTF8>(into output:inout UTF8, anchors:inout [(key:Anchor, index:UTF8.Index)]) 
         where UTF8:RangeReplaceableCollection, UTF8.Element == UInt8
     {
-        output.append(contentsOf: "<!DOCTYPE html>".utf8)
+        output.append(contentsOf: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>".utf8)
         self.element.rendered(into: &output, anchors: &anchors)
     }
 }
-extension DOM.Root where Domain == HTML, Anchor == Never
+extension DOM.Root where Domain == RSS, Anchor == Never
 {
     @inlinable public 
     func rendered<UTF8>(as type:UTF8.Type) -> UTF8
@@ -26,13 +26,13 @@ extension DOM.Root where Domain == HTML, Anchor == Never
 extension DOM.Template 
 {
     @inlinable public 
-    init(freezing html:DOM.Root<HTML, Key>) 
+    init(freezing html:DOM.Root<RSS, Key>) 
     {
         self.init()
         self.freeze(html)
     }
     @inlinable public mutating 
-    func freeze(_ html:DOM.Root<HTML, Key>)
+    func freeze(_ html:DOM.Root<RSS, Key>)
     {
         html.rendered(into: &self.literals, anchors: &self.anchors)
     }
